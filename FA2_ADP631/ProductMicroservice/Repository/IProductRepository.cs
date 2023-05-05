3314e2012fbfef741401d8803db59466c9f884fd
John Doe <arnoldfaku@gmail.com>
2023-05-05 23:52:05 +0200

    ProductMicroservice
    WeatherForecast

---
+++ FA2_ADP631/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductMicroservice.Models;
+using System.Collections.Generic;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductbyID(int productID);
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int productID);
+        void Save();
+        object GetProductByID(int id);
+    }
+}
